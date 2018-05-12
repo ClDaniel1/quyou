b42694985e909cad3b09f19a6c5a1a4bc3dcc9b3
ClDaniel1 <29816382+ClDaniel1@users.noreply.github.com>
2018-05-12 09:13:19 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '120.78.77.164',
+    'hostname'        => '',
     // 数据库名
-    'database'        => 'db_hf1707_quyou',
+    'database'        => '',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
-    'password'        => '12345678',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
